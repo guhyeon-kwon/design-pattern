1c0af7882bd93813638dbedd79dc58a1f26c5c49
GuHyeon <54255662+guhyeon-kwon@users.noreply.github.com>
2022-03-28 17:50:36 +0900

    commit

--- src/me/test/designpatterns/singleton/Settings.java
+++ src/me/test/designpatterns/singleton/Settings.java
@@ -1,18 +1,14 @@
 package me.test.designpatterns.singleton;
 
 public class Settings {
-    private static volatile Settings instance;
 
     private Settings() {};
 
-    public static Settings getInstance(){
-    if (instance == null){
-        synchronized (Setting.class){
-            if(instance == null){
-                instance = new Settings();
-            }
-        }
+    private static class SettingsHolder {
+        private static final Settings INSTANCE = new Settings();
     }
-    return instance;
+
+    public static Settings getInstance(){
+        return SettingsHolder.INSTANCE;
     }
 }
